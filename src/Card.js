1e5c7bf53a8cb9f9a4e5af44db975c6a158aba94
sebagruia <sebastiangruia@gmail.com>
2019-09-05 14:20:57 +0200

    First Commit

---
+++ src/Card.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Card = ({id, name, email})=>{ //Varianta cu destructurare direct ca parametru in functie
+// const Card = (props)=>{ Daca vreau sa folosesc "props" fara destructurarea de mai sus
+    // const {id, name, email} = props;  Asta e o varianta pentru destrcturare
+    return(
+        <div className="bg-light-green tc dib br3 pa3 ma2 grow bw2 shadow-5">
+            <img src={`https://robohash.org/${id}?200x200`} alt='robots'/>
+            <div>
+                <h2>{name}</h2>
+                <p>{email}</p>
+            </div>
+        </div>
+
+    );
+}
+
+export default Card;
