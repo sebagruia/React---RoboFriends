aadfe1330bd26838589814ec2cd47fcdb213c96f
sebagruia <sebastiangruia@gmail.com>
2019-10-17 21:45:34 +0200

    Updates

---
+++ precache-manifest.ee723e4ac41fdb14271d7d41b0a8d13d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02c0317bf1c168a13d2bc3b3ca555d2d",
+    "url": "/React---RoboFriends/index.html"
+  },
+  {
+    "revision": "366ebf2e33c246951c57",
+    "url": "/React---RoboFriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "4fc406aa2ed3da40ff92",
+    "url": "/React---RoboFriends/static/css/main.f63c2980.chunk.css"
+  },
+  {
+    "revision": "366ebf2e33c246951c57",
+    "url": "/React---RoboFriends/static/js/2.0e51a22d.chunk.js"
+  },
+  {
+    "revision": "4fc406aa2ed3da40ff92",
+    "url": "/React---RoboFriends/static/js/main.03d50504.chunk.js"
+  },
+  {
+    "revision": "b2a87b904dae8733a8ee",
+    "url": "/React---RoboFriends/static/js/runtime~main.23f39b39.js"
+  }
+]);
